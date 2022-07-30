e9fc2e4e1e67ee97340a714042046e5469eecac6
gen-ota <110288530+gen-ota@users.noreply.github.com>
2022-07-30 14:33:21 +0300

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,7 @@
+//to make a variabel
+var name =rt(1,3),fer = "qwer",cars=["ae","ew","fr"]//ary(list)
+var name =rt={we,ew,ew,we,r4,ew5wew,w65,yryt}
+//to make a function
+function rt(a, b){
+    return a + b;
+}
